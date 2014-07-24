2f4b070a947d96563e230a16823de13d416435f8
Lars Pastewka <lars.pastewka@iwm.fraunhofer.de>
2014-07-24 14:41:32 +0200

    Evaluation script compute work carried out by outer boundary.

--- scripts/fracture_mechanics/eval_energy_barrier.py
+++ scripts/fracture_mechanics/eval_energy_barrier.py
@@ -21,15 +21,93 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # ======================================================================
 
+import glob
+import sys
+
 import numpy as np
 from scipy.integrate import cumtrapz
 
+import ase.io
+from ase.data import atomic_numbers
+
 ###
 
-bond_length, force, epot = np.loadtxt('crack.out', unpack=True)
-epot_integrated = -cumtrapz(force, bond_length, initial=0.0)
-np.savetxt('crack_eval.out', np.transpose([bond_length,
-                                           force,
+sys.path += [ "." ]
+import params
+
+###
+
+# Atom types used for outputting the crack tip position.
+ACTUAL_CRACK_TIP = 'Au'
+FITTED_CRACK_TIP = 'Ag'
+
+###
+
+bond1, bond2 = params.bond
+
+bond_lengths = []
+
+fns = glob.glob('step_*.cfg')
+for fn in fns:
+    a = ase.io.read(fn)
+    del a[np.logical_or(a.numbers == atomic_numbers[ACTUAL_CRACK_TIP],
+                        a.numbers == atomic_numbers[FITTED_CRACK_TIP])]
+
+    # Bond length.
+    dr = a[bond1].position - a[bond2].position
+    bond_lengths += [ np.linalg.norm(dr) ]
+
+###
+
+indices = np.argsort(bond_lengths)
+
+epot_cluster = []
+bond_lengths = []
+bond_forces = []
+work = []
+
+last_a = None
+for fn in np.array(fns)[indices]:
+    a = ase.io.read(fn)
+    del a[np.logical_or(a.numbers == atomic_numbers[ACTUAL_CRACK_TIP],
+                        a.numbers == atomic_numbers[FITTED_CRACK_TIP])]
+
+    # Bond length.
+    dr = a[bond1].position - a[bond2].position
+    bond_lengths += [ np.linalg.norm(dr) ]
+
+    # Groups
+    g = a.get_array('groups')
+
+    # Get potential energy.
+    a.set_calculator(params.calc)
+    epot_cluster += [ a.get_potential_energy() ]
+
+    # Forces on bond.
+    forces = a.get_array('forces')
+    df = forces[bond1, :] - forces[bond2, :]
+    bond_forces += [ 0.5 * np.dot(df, dr)/np.sqrt(np.dot(dr, dr)) ]
+
+    # Work due to moving boundary.
+    if last_a is None:
+        work += [ 0.0 ]
+    else:
+        last_forces = last_a.get_array('forces')
+        # This is the trapezoidal rule.
+        work += [ np.sum(0.5 * (forces[g==0,:]+last_forces[g==0,:]) *
+                         (a.positions[g==0,:]-last_a.positions[g==0,:])
+                          ) ]
+
+    last_a = a
+
+# Sort according to bond length.
+epot_cluster = np.array(epot_cluster)-epot_cluster[0]
+work = np.cumsum(work)
+
+# Integrate true potential energy.
+epot = -cumtrapz(bond_forces, bond_lengths, initial=0.0)
+np.savetxt('crack_eval.out', np.transpose([bond_lengths,
+                                           bond_forces,
                                            epot,
-                                           epot-epot[0],
-                                           epot_integrated]))
+                                           epot_cluster,
+                                           work]))
