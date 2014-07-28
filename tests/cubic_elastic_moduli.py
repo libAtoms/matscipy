482813be6fab0728c69ccadeb453d65e71ce9065
Lars Pastewka <lars.pastewka@iwm.fraunhofer.de>
2014-07-28 13:46:06 +0200

    Cubic elastic moduli test for C11 and C12.

--- tests/cubic_elastic_moduli.py
+++ tests/cubic_elastic_moduli.py
@@ -34,6 +34,7 @@ from atomistica import Kumagai, TabulatedAlloyEAM
 
 from matscipy.elasticity import CubicElasticModuli
 from matscipy.elasticity import measure_orthorhombic_elastic_moduli
+from matscipy.elasticity import Voigt_6x6_to_orthorhombic
 
 ###
 
@@ -44,29 +45,27 @@ class TestCubicElasticModuli(unittest.TestCase):
 
     def test_rotation(self):
         for make_atoms, calc in [ 
-#            ( lambda a0,x : FaceCenteredCubic('Au', size=[1,1,1],
-#                                              latticeconstant=a0, directions=x),
-#              TabulatedAlloyEAM(fn='Au-Grochola-JCP05.eam.alloy') ),
+            ( lambda a0,x : FaceCenteredCubic('Au', size=[1,1,1],
+                                              latticeconstant=a0, directions=x),
+              TabulatedAlloyEAM(fn='Au-Grochola-JCP05.eam.alloy') ),
             ( lambda a0,x : Diamond('Si', size=[1,1,1], latticeconstant=a0,
                                     directions=x),
               Kumagai() )
             ]:
 
             a = make_atoms(None, [[1,0,0], [0,1,0], [0,0,1]])
-            print '=== {} ==='.format(a.get_chemical_symbols())
             a.set_calculator(calc)
             FIRE(StrainFilter(a, mask=[1,1,1,0,0,0]), logfile=None) \
                 .run(fmax=self.fmax)
             latticeconstant = np.mean(a.cell.diagonal())
 
-            C11, C12, C44 = measure_orthorhombic_elastic_moduli(a, #FIRE,
-                                                                delta=self.delta,
-                                                                fmax=self.fmax)
+            C11, C12, C44 = \
+                measure_orthorhombic_elastic_moduli(a, delta=self.delta,
+#                                                    optimizer=FIRE,
+                                                    fmax=self.fmax)
             C11 = np.mean(C11)/GPa
             C12 = np.mean(C12)/GPa
             C44 = np.mean(C44)/GPa
-
-            print C11, C12, C44
 
             el = CubicElasticModuli(C11, C12, C44)
 
@@ -83,49 +82,41 @@ class TestCubicElasticModuli(unittest.TestCase):
                 a.set_calculator(calc)
 
                 C11_m, C12_m, C44_m = \
-                    measure_orthorhombic_elastic_moduli(a, #FIRE,
-                                                        delta=self.delta,
+                    measure_orthorhombic_elastic_moduli(a, delta=self.delta,
+#                                                        optimizer=FIRE,
                                                         fmax=self.fmax)
                 C11_m /= GPa
                 C12_m /= GPa
                 C44_m /= GPa
+
+                C = el.rotate(directions)
+                C_check = el._rotate_explicit(directions)
+                self.assertTrue(np.all(np.abs(C-C_check) < 1e-6))
                 
-                A = el.rotate(directions)
-                #B = el._rotate_explicit(directions)
-                #C = el._rotate_explicit2(directions)
-                #assert np.all(np.abs(A-B) < 1e-6)
-                #assert np.all(np.abs(A-C) < 1e-6)
-
-                #print 'Check = ', compliance_matrix_to_elastic_constants(C)
-
-                #print el.compliance_matrix()
-
-                C11_rot, C12_rot, C44_rot = el.stiffness()
+                C11_rot, C12_rot, C44_rot = Voigt_6x6_to_orthorhombic(C)
 
                 dC11 = C11_m-C11_rot
                 dC12 = C12_m-C12_rot
                 dC44 = C44_m-C44_rot
-        
-                #if np.any(np.abs(dC11) > 1.0):
-                if 1:
-                    print '--- C11 ---'
-                    print C11_m
-                    print C11_rot
-                    print dC11
-                #if np.any(np.abs(dC12) > 1.0):
-                if 1:
-                    print '--- C12 ---'
-                    print C12_m
-                    print C12_rot
-                    print dC12
-                #if np.any(np.abs(dC44) > 1.0):
-                if 1:
-                    print '--- C44 ---'
-                    print C44_m
-                    print C44_rot
-                    print dC44
 
-                #print el.compliance_matrix()
+                if np.any(np.abs(dC11) > 1.0):
+                    print '--- C11 ---'
+                    print 'measured:   ', C11_m
+                    print 'rotated:    ', C11_rot
+                    print 'difference: ', dC11
+                if np.any(np.abs(dC12) > 1.0):
+                    print '--- C12 ---'
+                    print 'measured:   ', C12_m
+                    print 'rotated:    ', C12_rot
+                    print 'difference: ', dC12
+                if np.any(np.abs(dC44) > 1.0):
+                    print '--- C44 ---'
+                    print 'measured:   ', C44_m
+                    print 'rotated:    ', C44_rot
+                    print 'difference: ', dC44
+
+                self.assertTrue(np.all(np.abs(dC11) < 1e-2))
+                self.assertTrue(np.all(np.abs(dC12) < 1e-2))
 
 ###
 
