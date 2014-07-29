ae1cc07a058ec5618ac83df193234c0dba42647c
Lars Pastewka <lars.pastewka@iwm.fraunhofer.de>
2014-07-29 16:30:24 +0200

    Crack now in xy plane.

--- examples/fracture_mechanics/energy_barrier_multiple/params.py
+++ examples/fracture_mechanics/energy_barrier_multiple/params.py
@@ -24,7 +24,7 @@ C44             = 77.    # GPa
 surface_energy  = 1.08  * 10    # GPa*A = 0.1 J/m^2
 
 # Crack system
-n               = [ 6, 1, 6 ]
+n               = [ 6, 6, 1 ]
 crack_surface   = [ 1, 1, 0 ]
 crack_front     = [ 0, 0, 1 ]
 
@@ -37,8 +37,6 @@ k1              = 1.00
 bond_lengths    = np.linspace(2.5, 4.5, 41)
 
 fmax            = 0.001
-
-cutoff          = 5.0
 
 # Setup crack system
 cryst = diamond_110_001(el, a0, n, crack_surface, crack_front)
