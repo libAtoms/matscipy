34af1fdb4f6c9c8b994cb710b97fe0ed9e1311a7
James Kermode <james.kermode@gmail.com>
2014-08-11 10:57:37 +0100

    Add matscipy.fracture_mechanics to packages list to install subpackage

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='matscipy',
       maintainer_email='james.kermode@gmail.com',
       license='LGPLv2.1+',
       package_dir={'matscipy': 'matscipy'},
-      packages=['matscipy'],
+      packages=['matscipy', 'matscipy.fracture_mechanics'],
       scripts=scripts,
       ext_modules=[
         Extension(
