e7b7056a7808c6f895562f371742f6ea3cc1ed82
James Kermode <james.kermode@gmail.com>
2015-01-09 13:59:54 +0000

    Working example

--- examples/fracture_mechanics/energy_barrier/params.py
+++ examples/fracture_mechanics/energy_barrier/params.py
@@ -6,7 +6,7 @@ import numpy as np
 
 import ase.io
 
-from matscipy.fracture_mechanics.clusters import diamond_110_110
+from matscipy.fracture_mechanics.clusters import diamond_111_110
 
 import atomistica
 
@@ -24,21 +24,24 @@ C44             = 77.    # GPa
 surface_energy  = 1.08  * 10    # GPa*A = 0.1 J/m^2
 
 # Crack system
-n               = [ 16, 24, 1 ]
-crack_surface   = [ 1,-1, 0 ]
-crack_front     = [ 1, 1, 0 ]
+n               = [ 6, 4, 1 ]
+crack_surface   = [ 1, 1, 1 ]
+crack_front     = [ 1, -1, 0 ]
 
 vacuum          = 6.0
 
 # Simulation control
-bond            = ( 735, 800 )
-k1              = 1.10
-bond_lengths    = np.linspace(2.5, 5.0, 101)
+k1              = 0.900
+bond_lengths    = np.linspace(2.5, 4.5, 21)
 
 fmax            = 0.001
 
 cutoff          = 5.0
 
 # Setup crack system
-cryst = diamond_110_110(el, a0, n, crack_surface, crack_front)
+cryst = diamond_111_110(el, a0, n, crack_surface, crack_front)
 ase.io.write('cryst.cfg', cryst)
+
+optimize_tip_position = True
+
+basename = 'k_%04d' % int(k1*1000.0)
