4c8bb3a9d2e980a37c419e3d6c664842536032ad
Lars Pastewka <lars.pastewka@kit.edu>
2015-01-16 10:44:55 +0100

    Added JKR module.

---
+++ matscipy/contact_mechanics/JKR.py
@@ -0,0 +1,48 @@
+# ======================================================================
+# matscipy - Python materials science tools
+# https://github.com/libAtoms/matscipy
+#
+# Copyright (2014) James Kermode, King's College London
+#                  Lars Pastewka, Karlsruhe Institute of Technology
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# ======================================================================
+
+import math
+
+import numpy as np
+
+###
+
+def radius(N, R, Es, w):
+    """
+    Given normal load, sphere radius and contact modulus compute contact radius
+    and peak pressure.
+
+    Parameters
+    ----------
+    N : float
+        Normal force.
+    R : float
+        Sphere radius.
+    Es : float
+        Contact modulus: Es = E/(1-nu**2) with Young's modulus E and Poisson
+        number nu.
+    w : float
+        Work of adhesion.
+    """
+
+    return ( 3.*R/(4.*Es)*(N + 6*w*math.pi*R + np.sqrt(12*w*math.pi*R*N + 
+                           (6*w*math.pi*R)**2)) )**(1./3)
+
