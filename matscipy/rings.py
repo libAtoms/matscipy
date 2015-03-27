b17e7e8ed286ff2f0c4f2b5b1e87bcb92c7c2716
Lars Pastewka <lars.pastewka@iwm.fraunhofer.de>
2015-03-27 10:50:19 +0100

    Added rings convenience module.

---
+++ matscipy/rings.py
@@ -0,0 +1,50 @@
+# ======================================================================
+# matscipy - Python materials science tools
+# https://github.com/libAtoms/matscipy
+#
+# Copyright (2014) James Kermode, King's College London
+#                  Lars Pastewka, Karlsruhe Institute of Technology
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# ======================================================================
+
+import itertools
+
+import numpy as np
+
+from matscipy.neighbours import neighbour_list
+import _matscipy 
+
+###
+
+def ring_statistics(a, cutoff):
+    """
+    Compute number of shortest path rings in sample.
+    See: D.S. Franzblau, Phys. Rev. B 44, 4925 (1991)
+
+    Parameters
+    ----------
+    a : ase.Atoms
+        Atomic configuration.
+    cutoff : float
+        Cutoff for neighbor counting.
+
+    Returns
+    -------
+    ringstat : array
+        Array with number of shortest path rings.
+    """
+    i, j, r = neighbour_list('ijD', a, cutoff)
+    d = _matscipy.distance_map(i, j)
+    return _matscipy.find_sp_rings(i, j, r, d)
