61dd0f1acf101978f465d738e03c0f5af4309b02
Lars Pastewka <lars.pastewka@iwm.fraunhofer.de>
2015-05-12 12:40:53 +0200

    Tools: env.sh accepts Python interpreter to set environment for.

--- env.sh
+++ env.sh
@@ -1,6 +1,17 @@
 #! /bin/sh
 
+PYTHON="$1"
+if [ -z "$PYTHON" ]; then
+  PYTHON="python"
+fi
+
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PLATFORM=`python -c "from distutils.util import get_platform ; from distutils.sysconfig import get_python_version ; print '%s-%s' % ( get_platform(), get_python_version() )"`
+PLATFORM=`$PYTHON -c "from __future__ import print_function ; from distutils.util import get_platform ; from distutils.sysconfig import get_python_version ; print('{0}-{1}'.format(get_platform(), get_python_version()))"`
+
+echo "Setting Python environment"
+echo "--------------------------"
+echo "Python executable: $PYTHON"
+echo "Root directory:    $ROOT"
+echo "Platform:          $PLATFORM"
 
 export PYTHONPATH="$ROOT:$ROOT/build/lib.$PLATFORM:$PYTHONPATH"
