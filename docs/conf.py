3c9944f688a8a8ae65d2c1f2679ff4d40b50f016
Lars Pastewka <lars.pastewka@iwm.fraunhofer.de>
2015-05-19 09:48:33 +0200

    Documentation: numpy workaround for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -19,9 +19,9 @@ import os
 
 import mock
  
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+MOCK_MODULES = ['numpy']
 for mod_name in MOCK_MODULES:
-sys.modules[mod_name] = mock.Mock()
+    sys.modules[mod_name] = mock.Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
