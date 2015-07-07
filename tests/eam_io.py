81509fcfa0976932b13e6b664813fef93e466cb4
Lars Pastewka <lars.pastewka@kit.edu>
2015-07-07 13:46:05 +0200

    Testing: Turned EAM I/O test into a matsciptest. Also fixed some bugs.

--- tests/eam_io.py
+++ tests/eam_io.py
@@ -23,51 +23,51 @@
 # ======================================================================
 
 from __future__ import division
+
+import unittest
+
 import numpy as np
-from scipy import interpolate
 
 import os
-from matscipy.eam.io import (read_eam,read_eam_alloy,write_eam,write_eam_alloy)
+from matscipy.eam.io import read_eam,read_eam_alloy,write_eam,write_eam_alloy
 
-def test_eam_io():
-    source,parameters,F,f,rep = read_eam("Au_u3.eam")
-    write_eam(source,parameters,F,f,rep,"Au_u3_copy.eam")
-    source1,parameters1,F1,f1,rep1 = read_eam("Au_u3_copy.eam")
-    fail = 0
-    for i,p in enumerate(parameters):
-	if p != parameters1[i] :
-	    fail+=1
-    if F.any() != F1.any() :
-	fail+=1
-    if f.any() != f1.any() :
-	fail+=1
-    if rep.any() != rep1.any() :
-	fail+=1
-    if fail:
-	print "test fail eam_in != eam_out"
-    os.remove("Au_u3_copy.eam")
-    
-    source,parameters,F,f,rep = read_eam_alloy("CuAgNi_Zhou.eam.alloy")
-    write_eam_alloy(source,parameters,F,f,rep,"CuAgNi_Zhou.eam_copy.alloy")
-    source1,parameters1,F1,f1,rep1 = read_eam_alloy("CuAgNi_Zhou.eam_copy.alloy")
-    fail = 0
-    for i,p in enumerate(parameters):
-	try :
-	  for j,d in enumerate(p):
-	      if d != parameters[i][j]:
-		  fail+=1
-	except:
-	    if p != parameters[i]:
-		fail +=1
-    if F.any() != F1.any() :
-      fail+=1
-    if f.any() != f1.any() :
-      fail+=1
-    if rep.any() != rep1.any() :
-      fail+=1
-    if fail:
-      print "test fail eam_alloy_in != eam_alloy_out"
-    os.remove("CuAgNi_Zhou.eam_copy.alloy")
+import matscipytest
 
+###
+
+class TestEAMIO(matscipytest.MatSciPyTestCase):
+
+    def test_eam_read_write(self):
+        source,parameters,F,f,rep = read_eam("Au_u3.eam")
+        write_eam(source,parameters,F,f,rep,"Au_u3_copy.eam")
+        source1,parameters1,F1,f1,rep1 = read_eam("Au_u3_copy.eam")
+        os.remove("Au_u3_copy.eam")
+        for i,p in enumerate(parameters):
+            self.assertTrue(p == parameters1[i])
+        self.assertTrue((F == F1).all())
+        self.assertTrue((f == f1).all())
+        self.assertTrue((rep == rep1).all())
     
-test_eam_io()
+    def test_eam_alloy_read_write(self):
+        source,parameters,F,f,rep = read_eam_alloy("CuAgNi_Zhou.eam.alloy")
+        write_eam_alloy(source,parameters,F,f,rep,"CuAgNi_Zhou.eam_copy.alloy")
+        source1,parameters1,F1,f1,rep1 = read_eam_alloy("CuAgNi_Zhou.eam_copy.alloy")
+        os.remove("CuAgNi_Zhou.eam_copy.alloy")
+        fail = 0
+        for i,p in enumerate(parameters):
+            try:
+              for j,d in enumerate(p):
+                  if d != parameters[i][j]:
+                      fail+=1
+            except:
+                if p != parameters[i]:
+                    fail +=1
+        self.assertTrue(fail == 0)
+        self.assertTrue((F == F1).all())
+        self.assertTrue((f == f1).all())
+        self.assertTrue((rep == rep1).all())
+
+###
+
+if __name__ == '__main__':
+    unittest.main()
