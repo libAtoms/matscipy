909eb403d6aae4655a8943f377d491d15822fcbd
Lars Pastewka <lars.pastewka@kit.edu>
2015-07-09 20:53:35 +0200

    Testing: Added test for EAM stresses. Added test for Grochola parameterization.

--- tests/eam_calculator.py
+++ tests/eam_calculator.py
@@ -30,9 +30,50 @@ import numpy as np
 
 import ase.io as io
 from ase.calculators.test import numeric_force
+from ase.constraints import StrainFilter
+from ase.lattice.cubic import FaceCenteredCubic
+from ase.optimize import FIRE
+from ase.units import GPa
 
 import matscipytest
 from matscipy.eam.calculator import EAM
+from matscipy.elasticity import fit_elastic_constants, Voigt_6x6_to_cubic
+
+###
+
+def numeric_stress(atoms, d=1e-6):
+    """Evaluate stress tensor using finite differences.
+
+    This will trigger 18 calls to get_potential_energy(), with appropriate
+    cell deformation.
+    """
+    stress = np.zeros([3, 3])
+    cell0 = atoms.get_cell().copy()
+
+    for i in range(3):
+        for j in range(3):
+            cell = cell0.copy()
+            eps0 = np.eye(3)
+            eps = eps0.copy()
+
+            eps[i, j] = eps0[i, j]-d
+            cell = np.dot(cell0, eps)
+            atoms.set_cell(cell, scale_atoms=True)
+            e1 = atoms.get_potential_energy()
+
+            eps[i, j] = eps0[i, j]+d
+            cell = np.dot(cell0, eps)
+            atoms.set_cell(cell, scale_atoms=True)
+            e2 = atoms.get_potential_energy()
+
+            stress[i, j] = (e2-e1)/(2*d)
+
+    atoms.set_cell(cell0, scale_atoms=True)
+
+    return np.array([stress[0,0], stress[1,1], stress[2,2],
+                     (stress[1,2]+stress[2,1])/2,
+                     (stress[0,2]+stress[2,0])/2,
+                     (stress[0,1]+stress[1,0])/2])/atoms.get_volume()
 
 ###
 
@@ -53,6 +94,25 @@ class TestEAMCalculator(matscipytest.MatSciPyTestCase):
                 d = random.randrange(3)
                 self.assertTrue((numeric_force(a, i, d, self.disp)-f[i, d]) <
                                 self.tol)
+
+    def test_stress(self):
+        a = FaceCenteredCubic('Au', size=[2,2,2])
+        calc = EAM('Au-Grochola-JCP05.eam.alloy')
+        a.set_calculator(calc)
+        self.assertArrayAlmostEqual(a.get_stress(), numeric_stress(a))
+
+    def test_Grochola(self):
+        a = FaceCenteredCubic('Au', size=[2,2,2])
+        calc = EAM('Au-Grochola-JCP05.eam.alloy')
+        a.set_calculator(calc)
+        FIRE(StrainFilter(a, mask=[1,1,1,0,0,0]), logfile=None).run(fmax=0.001)
+        a0 = a.cell.diagonal().mean()/2
+        self.assertTrue(abs(a0-4.0701)<2e-5)
+        self.assertTrue(abs(a.get_potential_energy()/len(a)+3.924)<0.0003)
+        C, C_err = fit_elastic_constants(a, symmetry='cubic', verbose=False)
+        C11, C12, C44 = Voigt_6x6_to_cubic(C)
+        self.assertTrue(abs((C11-C12)/GPa-32.07)<0.7)
+        self.assertTrue(abs(C44/GPa-45.94)<0.5)
 
 ###
 
