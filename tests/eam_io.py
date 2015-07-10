61679d7b4a3fb27fcff83bee6122fbb3851db4f2
gola <gola@cmsquad25.iam-cms.kit.edu>
2015-07-10 15:51:06 +0200

    added test for eam.io mix_eam_alloy

--- tests/eam_io.py
+++ tests/eam_io.py
@@ -29,7 +29,15 @@ import unittest
 import numpy as np
 
 import os
-from matscipy.eam.io import read_eam,read_eam_alloy,write_eam,write_eam_alloy
+from matscipy.eam.io import read_eam,read_eam_alloy,write_eam,write_eam_alloy,mix_eam_alloy
+from matscipy.eam.calculator import EAM
+
+import ase.io as io
+from ase.calculators.test import numeric_force
+from ase.constraints import StrainFilter, UnitCellFilter
+from ase.lattice.compounds import B1, B2, L1_0, L1_2
+from ase.lattice.cubic import FaceCenteredCubic
+from ase.optimize import FIRE
 
 import matscipytest
 
@@ -78,6 +86,66 @@ class TestEAMIO(matscipytest.MatSciPyTestCase):
             for j in range(len(rep)):
                 if j < i :
                     self.assertTrue((rep[i,j,:] == rep1[i,j,:]).all())
+         
+    def test_mix_eam_alloy(self):
+        source,parameters,F,f,rep = read_eam_alloy("CuAu_Zhou.eam.alloy")
+        source1,parameters1,F1,f1,rep1 = mix_eam_alloy(["Cu_Zhou.eam.alloy","Au_Zhou.eam.alloy"],"weight")
+        write_eam_alloy(source1,parameters1,F1,f1,rep1,"CuAu_mixed.eam.alloy")
+
+        calc0 = EAM('CuAu_Zhou.eam.alloy')
+        calc1 = EAM('CuAu_mixed.eam.alloy')
+      
+        a = FaceCenteredCubic('Cu', size=[2,2,2])
+        a.set_calculator(calc0)
+        FIRE(StrainFilter(a, mask=[1,1,1,0,0,0]), logfile=None).run(fmax=0.001)
+        e0 = a.get_potential_energy()/len(a)
+        a = FaceCenteredCubic('Cu', size=[2,2,2])
+        a.set_calculator(calc1)
+        FIRE(StrainFilter(a, mask=[1,1,1,0,0,0]), logfile=None).run(fmax=0.001)
+        e1 = a.get_potential_energy()/len(a)
+        self.assertTrue(e0-e1 < 0.0005)
+
+        a = FaceCenteredCubic('Au', size=[2,2,2])
+        a.set_calculator(calc0)
+        FIRE(StrainFilter(a, mask=[1,1,1,0,0,0]), logfile=None).run(fmax=0.001)
+        e0 = a.get_potential_energy()/len(a)
+        a = FaceCenteredCubic('Au', size=[2,2,2])
+        a.set_calculator(calc1)
+        FIRE(StrainFilter(a, mask=[1,1,1,0,0,0]), logfile=None).run(fmax=0.001)
+        e1 = a.get_potential_energy()/len(a)
+        self.assertTrue(e0-e1 < 0.0005)
+
+        a = L1_2(['Au', 'Cu'], size=[2,2,2], latticeconstant=4.0)
+        a.set_calculator(calc0)
+        FIRE(UnitCellFilter(a, mask=[1,1,1,0,0,0]), logfile=None).run(fmax=0.001)
+        e0 = a.get_potential_energy()/len(a)
+        a = L1_2(['Au', 'Cu'], size=[2,2,2], latticeconstant=4.0)
+        a.set_calculator(calc1)
+        FIRE(UnitCellFilter(a, mask=[1,1,1,0,0,0]), logfile=None).run(fmax=0.001)
+        e1 = a.get_potential_energy()/len(a)
+        self.assertTrue(e0-e1 < 0.0005)
+
+        a = L1_2(['Cu', 'Au'], size=[2,2,2], latticeconstant=4.0)
+        a.set_calculator(calc0)
+        FIRE(UnitCellFilter(a, mask=[1,1,1,0,0,0]), logfile=None).run(fmax=0.001)
+        e0 = a.get_potential_energy()/len(a)
+        a = L1_2(['Cu', 'Au'], size=[2,2,2], latticeconstant=4.0)
+        a.set_calculator(calc1)
+        FIRE(UnitCellFilter(a, mask=[1,1,1,0,0,0]), logfile=None).run(fmax=0.001)
+        e1 = a.get_potential_energy()/len(a)
+        self.assertTrue(e0-e1 < 0.0005)
+
+        a = B1(['Au', 'Cu'], size=[2,2,2], latticeconstant=4.0)
+        a.set_calculator(calc0)
+        FIRE(UnitCellFilter(a, mask=[1,1,1,0,0,0]), logfile=None).run(fmax=0.001)
+        e0 = a.get_potential_energy()/len(a)
+        a = B1(['Au', 'Cu'], size=[2,2,2], latticeconstant=4.0)
+        a.set_calculator(calc1)
+        FIRE(UnitCellFilter(a, mask=[1,1,1,0,0,0]), logfile=None).run(fmax=0.001)
+        e1 = a.get_potential_energy()/len(a)
+        self.assertTrue(e0-e1 < 0.0005)
+        
+        os.remove("CuAu_mixed.eam.alloy")
 
 ###
 
