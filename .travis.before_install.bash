c8034f96e4f8d4ff029735c3c582167d0f1c420e
James Kermode <james.kermode@gmail.com>
2015-09-09 12:51:49 +0100

    use homebrew gcc/g++ not Apple ones on osx

--- .travis.before_install.bash
+++ .travis.before_install.bash
@@ -3,6 +3,7 @@
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   echo "OK"
 elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
+  brew install gcc
   if [ "$PYTHON" == "/usr/local/bin/python3" ]; then
     brew install python3
     brew link --overwrite python
