1cfbcd379cb0d744b316db09547e3fc6d9f62a39
James Kermode <james.kermode@gmail.com>
2015-09-09 12:42:10 +0100

    further tweaks to Travis osx config

--- .travis.before_install.bash
+++ .travis.before_install.bash
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-  echo "AOK"
-  #if [ "$PYTHON" == "/usr/bin/python3.4" ]; then
-    #sudo add-apt-repository ppa:fkrull/deadsnakes -y
-    #sudo apt-get update
-    #sudo apt-get install python3.4 python3-dev
-  #fi
+  echo "OK"
 elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
   if [ "$PYTHON" == "/usr/local/bin/python3" ]; then
     brew install python3
+  elif [ "$PYTHON" == "/usr/local/bin/python" ]; then
+    brew install python
   fi
   sudo pip install virtualenv
   virtualenv -p $PYTHON venv
--- .travis.before_script.bash
+++ .travis.before_script.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-  sudo apt-get install cmake libgtest-dev build-essential python-setuptools
+  sudo apt-get install python-setuptools
 elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
   # noop?
   pip install setuptools
