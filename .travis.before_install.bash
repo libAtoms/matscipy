83149815d66d11046dfde9377da72d87b8aa50f0
James Kermode <james.kermode@gmail.com>
2015-09-09 12:46:37 +0100

    further tweaks to Travis osx config

--- .travis.before_install.bash
+++ .travis.before_install.bash
@@ -5,8 +5,10 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
   if [ "$PYTHON" == "/usr/local/bin/python3" ]; then
     brew install python3
+    brew link --overwrite python
   elif [ "$PYTHON" == "/usr/local/bin/python" ]; then
     brew install python
+    brew link --overwrite python
   fi
   sudo pip install virtualenv
   virtualenv -p $PYTHON venv
