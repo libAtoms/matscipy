c75e69a04f31896eee3e3228f3d3e1b75ee42e29
James Kermode <james.kermode@gmail.com>
2015-09-09 12:57:24 +0100

    link gcc and g++ after brew install. temp disable emails...

--- .travis.before_install.bash
+++ .travis.before_install.bash
@@ -4,6 +4,7 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   echo "OK"
 elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
   brew install gcc
+  brew link --overwrite gcc
   if [ "$PYTHON" == "/usr/local/bin/python3" ]; then
     brew install python3
     brew link --overwrite python
