34a151ea96bd16b58436db4e7b6a2e8a7b89c6d7
Lars Pastewka <lars.pastewka@kit.edu>
2017-03-01 16:28:56 +0000

    TST: Added test to check convergence of bond length (strain) with size of embedded region.

--- tests/crack_tests.py
+++ tests/crack_tests.py
@@ -3,15 +3,21 @@ import unittest
 import numpy as np
 import ase.io
 import ase.units as units
-from ase.constraints import FixAtoms
+from ase.build import bulk
+from ase.constraints import FixAtoms, UnitCellFilter
 from ase.md.verlet import VelocityVerlet
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
+from ase.optimize import FIRE
 
-from matscipy.fracture_mechanics.crack import (ConstantStrainRate)
+import matscipy.fracture_mechanics.crack as crack
+from matscipy.elasticity import fit_elastic_constants
+from matscipy.fracture_mechanics.crack import ConstantStrainRate
+from matscipy.fracture_mechanics.clusters import diamond, set_groups
 
 try:
     import atomistica
     from atomistica import TersoffScr, Tersoff_PRB_39_5566_Si_C__Scr
+    from atomistica import Tersoff, Tersoff_PRB_39_5566_Si_C
     have_atomistica = True
 except ImportError:
     have_atomistica = False
@@ -70,6 +76,97 @@ if have_atomistica:
 
             temperature = (atoms.get_kinetic_energy()/(1.5*units.kB*len(atoms)))
             self.assertAlmostEqual(temperature, temp_answer)
+
+        def test_embedding_size_convergence(self):
+            calc = Tersoff(**Tersoff_PRB_39_5566_Si_C)
+
+            el = 'C'
+            a0 = 3.566
+            surface_energy = 2.7326 * 10
+            crack_surface = [1, 1, 1]
+            crack_front = [1, -1, 0]
+            skin_x, skin_y = 1, 1
+
+            cryst = bulk(el, cubic=True)
+            cryst.set_calculator(calc)
+            FIRE(UnitCellFilter(cryst), logfile=None).run(fmax=1e-6)
+            a0 = cryst.cell.diagonal().mean()
+            bondlength = cryst.get_distance(0, 1)
+            #print('a0 =', a0, ', bondlength =', bondlength)
+
+            cryst = diamond(el, a0, [1,1,1], crack_surface, crack_front)
+            cryst.set_pbc(True)
+            cryst.set_calculator(calc)
+            cryst.set_cell(cryst.cell.diagonal(), scale_atoms=True)
+
+            C, C_err = fit_elastic_constants(cryst, verbose=False,
+                                             symmetry='cubic',
+                                             optimizer=FIRE,
+                                             fmax=1e-6)
+            #print('Measured elastic constants (in GPa):')
+            #print(np.round(C*10/units.GPa)/10)
+
+            bondlengths = []
+            for i, n in enumerate([[6, 5, 1], [11, 9, 1], [21, 19, 1],
+                                   [41, 39, 1]]):
+                #print(n)
+                cryst = diamond(el, a0, n, crack_surface, crack_front)
+                set_groups(cryst, n, skin_x, skin_y)
+                cryst.set_pbc(True)
+                cryst.set_calculator(calc)
+                FIRE(UnitCellFilter(cryst), logfile=None).run(fmax=1e-6)
+                cryst.set_cell(cryst.cell.diagonal(), scale_atoms=True)
+
+                ase.io.write('cryst_{}.xyz'.format(i), cryst, format='extxyz')
+
+                crk = crack.CubicCrystalCrack(crack_surface,
+                                              crack_front,
+                                              Crot=C/units.GPa)
+                k1g = crk.k1g(surface_energy)
+
+                tip_x = cryst.cell.diagonal()[0]/2
+                tip_y = cryst.cell.diagonal()[1]/2
+
+                a = cryst.copy()
+                a.set_pbc([False, False, True])
+
+                k1 = 1.0
+                ux, uy = crk.displacements(cryst.positions[:,0], cryst.positions[:,1],
+                                           tip_x, tip_y, k1*k1g)
+
+                a.positions[:, 0] += ux
+                a.positions[:, 1] += uy
+
+                # Center notched configuration in simulation cell and ensure enough vacuum.
+                oldr = a[0].position.copy()
+                a.center(vacuum=10.0, axis=0)
+                a.center(vacuum=10.0, axis=1)
+                tip_x += a[0].x - oldr[0]
+                tip_y += a[0].y - oldr[1]
+
+                # Move reference crystal by same amount
+                cryst.set_cell(a.cell)
+                cryst.set_pbc([False, False, True])
+                cryst.translate(a[0].position - oldr)
+
+                bond1, bond2 = crack.find_tip_coordination(a, bondlength=bondlength*1.2)
+
+                # Groups mark the fixed region and the region use for fitting the crack tip.
+                g = a.get_array('groups')
+                gcryst = cryst.get_array('groups')
+
+                ase.io.write('cryst_{}.xyz'.format(i), cryst)
+
+                a.set_calculator(calc)
+                a.set_constraint(FixAtoms(mask=g==0))
+                FIRE(a, logfile=None).run(fmax=1e-3)
+
+                ase.io.write('crack_{}.xyz'.format(i), a)
+                bondlengths += [a.get_distance(bond1, bond2)]
+            print(bondlengths, np.diff(bondlengths), bondlengths/bondlengths[-1]-1)
+            assert np.all(np.diff(bondlengths) < 0)
+            assert np.max(bondlengths/bondlengths[-1]-1) < 0.01
+
 
 if __name__ == '__main__':
     unittest.main()
