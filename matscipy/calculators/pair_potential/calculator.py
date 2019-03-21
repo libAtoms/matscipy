nat1 defines the number of rows in the hessian matrix. Corrected the calculation of the hessian matrix by introducing the paramter nat1.

matscipy/calculators/pair_potential/calculator.py
@@ -418,16 +418,16 @@ class PairPotential(Calculator):
             H_ncc += -(de_n/abs_dr_n * (np.eye(3, dtype=e_nc.dtype) -
                                         (e_nc.reshape(-1, 3, 1) * e_nc.reshape(-1, 1, 3))).T).T
 
-           	H = bsr_matrix((H_ncc, j_n, first_i), shape=(3*nat, 3*nat))
+           	H = bsr_matrix((H_ncc, j_n, first_i), shape=(3*nat1, 3*nat))
 
-            Hdiag_icc = np.empty((nat, 3, 3))
+            Hdiag_icc = np.empty((nat1, 3, 3))
             for x in range(3):
                 for y in range(3):
                     Hdiag_icc[:, x, y] = - \
                         np.bincount(i_n, weights=H_ncc[:, x, y])
 
-            H += bsr_matrix((Hdiag_icc, np.arange(nat),
-                             np.arange(nat+1)), shape=(3*nat, 3*nat))
+            H += bsr_matrix((Hdiag_icc, np.arange(nat1),
+                             np.arange(nat1+1)), shape=(3*nat1, 3*nat))
             return H
 
         # Dense matrix format
@@ -438,18 +438,18 @@ class PairPotential(Calculator):
             H_ncc += -(de_n/abs_dr_n * (np.eye(3, dtype=e_nc.dtype) -
                                         (e_nc.reshape(-1, 3, 1) * e_nc.reshape(-1, 1, 3))).T).T
 
-            H = np.zeros((3*nat, 3*nat))
+            H = np.zeros((3*nat1, 3*nat))
             for atom in range(len(i_n)):
                 H[H_ncc.shape[1]*i_n[atom]:H_ncc.shape[1]*i_n[atom]+H_ncc.shape[1], H_ncc.shape[2]*j_n[atom]:H_ncc.shape[2]*j_n[atom]
                   + H_ncc.shape[2]] = H_ncc[atom]
 
-            Hdiag_icc = np.empty((nat, 3, 3))
+            Hdiag_icc = np.empty((nat1, 3, 3))
             for x in range(3):
                 for y in range(3):
                     Hdiag_icc[:, x, y] = - \
                         np.bincount(i_n, weights=H_ncc[:, x, y])
 
-            Hdiag_ncc = np.zeros((3*nat, 3*nat))
+            Hdiag_ncc = np.zeros((3*nat1, 3*nat))
             for atom in range(nat):
                 Hdiag_ncc[Hdiag_icc.shape[1]*atom:Hdiag_icc.shape[1]*atom+Hdiag_icc.shape[1], Hdiag_icc.shape[2]*atom:Hdiag_icc.shape[2]*atom
                           + Hdiag_icc.shape[2]] = Hdiag_icc[atom]