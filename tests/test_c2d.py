5449950e02343112bb20d5ee17fbef2cf82a89f6
Johannes Hörmann <johannes.hoermann@imtek.uni-freiburg.de>
2019-12-12 19:21:40 +0100

    Minimal requirements.txt, skipping test_c2d.py:test_c2d_input_format_npz_output_format_lammps unless ASE version > 3.18.1

--- tests/test_c2d.py
+++ tests/test_c2d.py
@@ -9,6 +9,8 @@ import subprocess
 import tempfile
 import unittest
 
+from distutils.version import LooseVersion
+
 class c2dCliTest(matscipytest.MatSciPyTestCase):
     """Tests c2d and pnp command line interfaces"""
 
@@ -94,6 +96,8 @@ class c2dCliTest(matscipytest.MatSciPyTestCase):
                 ==  self.ref_xyz.get_initial_charges() ).all() )
             self.assertTrue( ( xyz.cell ==  self.ref_xyz.cell ).all() )
 
+    @unittest.skipUnless(LooseVersion(ase.__version__) > LooseVersion('3.18.1'),
+        "LAMMPS data file won't work for ASE version up until 3.18.1, skipped")
     def test_c2d_input_format_npz_output_format_lammps(self):
         """c2d NaCl.npz NaCl.lammps"""
         print("  RUN test_c2d_input_format_npz_output_format_lammps")
@@ -174,8 +178,8 @@ class c2dCliTest(matscipytest.MatSciPyTestCase):
             self.assertEqual(pnp.wait(),0)
 
             c2d_output = c2d.communicate()[0]
-            xyz_instream = io.StringIO(c2d_output)
-            xyz = ase.io.read(xyz_instream, format='xyz')
+            with io.StringIO(c2d_output) as xyz_instream:
+                xyz = ase.io.read(xyz_instream, format='xyz')
             self.assertEqual(len(xyz),len(self.ref_xyz))
             self.assertTrue( ( xyz.symbols == self.ref_xyz.symbols ).all() )
             self.assertTrue( ( xyz.get_initial_charges()
