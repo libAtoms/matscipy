648c4a5872c722126fa2ec9f7229405a1d012e88
Jan Griesser <jan.griesser@imtek.uni-freiburg.de>
2020-08-12 10:37:53 +0200

    - Added class IPL for the computation of the properties of a inverse-power-law with exponent 10.  - Added methods which return cutoff, first and second derivative.

---
+++ matscipy/calculators/polydisperse/calculator.py
@@ -0,0 +1,65 @@
+# ======================================================================
+# matscipy - Python materials science tools
+# https://github.com/libAtoms/matscipy
+#
+# Copyright (2014-2018) James Kermode, King's College London
+#                       Lars Pastewka, University of Freiburg
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# ======================================================================
+
+from __future__ import division
+
+import os
+
+import sys
+
+import time
+
+import numpy as np
+
+import ase
+from ase.calculators.calculator import Calculator
+
+from matscipy.neighbours import neighbour_list, first_neighbours
+
+###
+
+class IPL():
+	"""
+    Functional form for a inverse-power-law potential with an exponent of 10.
+    Energy, its first, second and third derivative are shifted to zero at cutoff.
+	"""
+    
+    def __init__(self, epsilon, cutoff):
+        self.epsilon = epsilon
+        self.cutoff = cutoff
+
+    def get_cutoff(self):
+        return self.cutoff
+
+    def first_derivative(self, r):
+
+    def second_derivative(self, r):
+
+    def derivative(self, n=1):
+        if n == 1:
+            return self.first_derivative
+        elif n == 2:
+            return self.second_derivative
+        else:
+            raise ValueError(
+                "Don't know how to compute {}-th derivative.".format(n))     	
+
+###
