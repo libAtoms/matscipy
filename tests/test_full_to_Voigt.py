21e4caf29d7743b74207b1a91e51e23be2c7c36b
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2020-11-17 17:00:34 +0100

    TST: Added test for stress and strain conversion

--- tests/test_full_to_Voigt.py
+++ tests/test_full_to_Voigt.py
@@ -25,32 +25,43 @@ import unittest
 
 import numpy as np
 
-import matscipytest
-from matscipy.elasticity import (full_3x3_to_Voigt_6_index, 
+from matscipy.elasticity import (full_3x3_to_Voigt_6_index,
                                  Voigt_6x6_to_full_3x3x3x3,
-                                 full_3x3x3x3_to_Voigt_6x6)
+                                 full_3x3x3x3_to_Voigt_6x6,
+                                 Voigt_6_to_full_3x3_strain,
+                                 full_3x3_to_Voigt_6_strain,
+                                 Voigt_6_to_full_3x3_stress,
+                                 full_3x3_to_Voigt_6_stress)
+
 
 ###
 
-class TestFullToVoigt(matscipytest.MatSciPyTestCase):
+def test_full_3x3_to_Voigt_6_index():
+    assert full_3x3_to_Voigt_6_index(1, 1) == 1
+    assert full_3x3_to_Voigt_6_index(1, 2) == 3
+    assert full_3x3_to_Voigt_6_index(2, 1) == 3
+    assert full_3x3_to_Voigt_6_index(0, 2) == 4
+    assert full_3x3_to_Voigt_6_index(0, 1) == 5
 
-    def test_full_3x3_to_Voigt_6_index(self):
-        self.assertTrue(full_3x3_to_Voigt_6_index(1, 1) == 1)
-        self.assertTrue(full_3x3_to_Voigt_6_index(1, 2) == 3)
-        self.assertTrue(full_3x3_to_Voigt_6_index(2, 1) == 3)
-        self.assertTrue(full_3x3_to_Voigt_6_index(0, 2) == 4)
-        self.assertTrue(full_3x3_to_Voigt_6_index(0, 1) == 5)
 
-    def test_stiffness_conversion(self):
-        C6 = np.random.random((6,6))
-        # Should be symmetric
-        C6 = (C6+C6.T)/2
-        C3x3 = Voigt_6x6_to_full_3x3x3x3(C6)
-        C6_check = full_3x3x3x3_to_Voigt_6x6(C3x3)
-        self.assertArrayAlmostEqual(C6, C6_check)
+def test_stiffness_conversion():
+    C6 = np.random.random((6, 6))
+    # Should be symmetric
+    C6 = (C6 + C6.T) / 2
+    C3x3 = Voigt_6x6_to_full_3x3x3x3(C6)
+    C6_check = full_3x3x3x3_to_Voigt_6x6(C3x3)
+    np.testing.assert_array_almost_equal(C6, C6_check)
 
-###
 
-if __name__ == '__main__':
-    unittest.main()
+def test_strain_conversion():
+    voigt0 = np.random.random(6)
+    full1 = Voigt_6_to_full_3x3_strain(voigt0)
+    voigt1 = full_3x3_to_Voigt_6_strain(full1)
+    np.testing.assert_array_almost_equal(voigt0, voigt1)
 
+
+def test_stress_conversion():
+    voigt0 = np.random.random(6)
+    full2 = Voigt_6_to_full_3x3_stress(voigt0)
+    voigt2 = full_3x3_to_Voigt_6_stress(full2)
+    np.testing.assert_array_almost_equal(voigt0, voigt2)
