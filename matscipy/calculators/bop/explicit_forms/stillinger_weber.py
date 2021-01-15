750754427154e5720809f95c28c1ceafea4828ce
griessej <fr_jg1080@login1.nemo.privat>
2021-01-15 15:56:14 +0100

    Added correct SW parameters.

--- matscipy/calculators/bop/explicit_forms/stillinger_weber.py
+++ matscipy/calculators/bop/explicit_forms/stillinger_weber.py
@@ -5,18 +5,27 @@ def ab(x):
     return np.linalg.norm(x, axis=1)
 
 def StillingerWeber():
-    A = 3281.5905
-    B = 121.00047
-    lambda_1 = 3.2300135
-    lambda_2 = 1.3457970
-    eta = 1.0000000
-    delta = 0.53298909
-    alpha = 2.3890327
-    c_1 = 0.20173476
-    c_2 = 730418.72
-    c_3 = 1000000.0
-    c_4 = 1.0000000
-    c_5 = 26.000000
-    h = -0.36500000
-    R_1 = 2.70
-    R_2 = 3.30
+    """
+
+    Reference
+    ------------
+    F. Stillinger and T. Weber, Physical review B 31.8 5262 (1985)
+
+    """
+    epsilon = 2.1683         
+    sigma = 2.0951         
+    costheta0 = -0.333333333333
+    A = 7.049556277    
+    B = 0.6022245584   
+    p = 4              
+    q = 0              
+    a = 1.80           
+    lambda_1 = 21.0           
+    gamma = 1.20           
+
+
+
+
+
+
+
