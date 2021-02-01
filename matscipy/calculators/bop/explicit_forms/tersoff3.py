53d9eb2309abf8948159d67445d55f6227300e1b
griesserj <griesserj@fp-10-126-131-161.eduroam-fp.privat>
2021-02-01 17:15:36 +0100

    Started to transform T3 potential to vectorized form

--- matscipy/calculators/bop/explicit_forms/tersoff3.py
+++ matscipy/calculators/bop/explicit_forms/tersoff3.py
@@ -12,34 +12,34 @@ def TersoffIII():
     c = 1.0039e5
     d = 1.6217e1
     h = -5.9825e-1
-    r1 = 2.70
-    r2 = 3.00
+    R_1 = 2.70
+    R_2 = 3.00
     #lam3 = 5.19745
     lam3 = 0.0
     delta = 3
 
     f = lambda r: np.where(
-        r < r1,
+        r < R_1,
         np.ones_like(r),
-        np.where(r > r2,
+        np.where(r > R_2,
             np.zeros_like(r),
-            (1 + np.cos((np.pi*(r-r1)/(r2-r1))))/2
+            (1 + np.cos((np.pi*(r-R_1)/(R_2-r1))))/2
             )
         )
     df = lambda r: np.where(
-        r < r1,
+        r < R_1,
         np.zeros_like(r),
-        np.where(r > r2,
+        np.where(r > R_2,
             np.zeros_like(r),
-            -np.pi * np.sin(np.pi * (r - r1)/(r2 - r1)) / (2*(r2 - r1))
+            -np.pi * np.sin(np.pi * (r - R_1)/(R_2 - R_1)) / (2*(R_2 - R_1))
             )
         )
     ddf = lambda r: np.where(
-        r < r1,
+        r < R_1,
         np.zeros_like(r),
-        np.where(r > r2,
+        np.where(r > R_2,
             np.zeros_like(r),
-            -np.pi**2 * np.cos(np.pi * (r - r1)/(r2 - r1)) / (2*(r2 - r1)**2)
+            -np.pi**2 * np.cos(np.pi * (r - R_1)/(R_2 - R_1)) / (2*(R_2 - R_1)**2)
             )
         )
 
@@ -81,100 +81,67 @@ def TersoffIII():
          + lam3**2 * hf(rij, rik))
 
     costh = lambda rij, rik: np.sum(rij*rik, axis=1) / (ab(rij)*ab(rik))
-    c1q = lambda rij, rik, q: (rik[:, q]/ab(rik) - rij[:, q]/ab(rij) * costh(rij, rik)) / ab(rij)
-    c2q = lambda rij, rik, q: (rij[:, q]/ab(rij) - rik[:, q]/ab(rik) * costh(rij, rik)) / ab(rik)
 
-    dc1q1t = lambda rij, rik, q, t: \
-        (- c1q(rij, rik, q) * rij[:, t] \
-         - rij[:, q] * c1q(rij, rik, t) \
-         - costh(rij, rik) * (int(q == t) - rij[:, q]*rij[:, t]/ab(rij)**2) \
-        )/ab(rij)**2
-    dc2q2t = lambda rij, rik, q, t: \
-        (- c2q(rij, rik, q) * rik[:, t] \
-         - rik[:, q] * c2q(rij, rik, t) \
-         - costh(rij, rik) * (int(q == t) - rik[:, q]*rik[:, t]/ab(rik)**2) \
-        )/ab(rik)**2
-    dc1q2t = lambda rij, rik, q, t: \
-        ((int(q == t) - rij[:, q]*rij[:, t]/ab(rij)**2)/ab(rij)
-         - c1q(rij, rik, q) * rik[:, t]/ab(rik) \
-        )/ab(rik)
+    c1 = lambda rij, rik: ((rik.T/ab(rik) - rij.T/ab(rij) * costh(rij, rik)) / ab(rij)).T
+    c2 = lambda rij, rik: ((rij.T/ab(rij) - rik.T/ab(rik) * costh(rij, rik)) / ab(rik)).T
 
-    Dh1q = lambda rij, rik, q: d1h(rij, rik) * (rij[:, q] / ab(rij))
-    Dh2q = lambda rij, rik, q: d2h(rij, rik) * (rik[:, q] / ab(rik))
+    dc11 = lambda rij, rik: \
+        ((- c1(rij, rik).reshape(-1, 3, 1) * rij.reshape(-1, 1, 3) \
+         - rij.reshape(-1, 3, 1) * c1(rij, rik).reshape(-1, 1, 3) \
+         - (costh(rij, rik) * (np.eye(3) -  ((rij.reshape(-1, 1, 3)*rij.reshape(-1, 3, 1)).T/ab(rij)**2).T).T).T \
+        ).T/ab(rij)**2).T
+    dc22 = lambda rij, rik: \
+        ((- c2(rij, rik).reshape(-1, 3, 1) * rik.reshape(-1, 1, 3) \
+         - rik.reshape(-1, 3, 1) * c2(rij, rik).reshape(-1, 1, 3) \
+         - (costh(rij, rik) * (np.eye(3) -  ((rik.reshape(-1, 1, 3)*rik.reshape(-1, 3, 1)).T/ab(rik)**2).T).T).T \
+        ).T/ab(rik)**2).T
+    
+    dc12 = lambda rij, rik: \
+        (((np.eye(3) -  ((rij.reshape(-1, 1, 3)*rij.reshape(-1, 3, 1)).T/ab(rij)**2).T).T/ab(rij)
+         - (c1(rij, rik).reshape(-1, 3, 1) * rik.reshape(-1, 1, 3)).T/ab(rik) \
+        )/ab(rik)).T
 
-    Dh1q1t = lambda rij, rik, q, t: \
-        d11h(rij, rik) * rij[:, q]/ab(rij) * rij[:, t]/ab(rij) \
-         + d1h(rij, rik) * (int(q == t) - rij[:, q]/ab(rij) * rij[:, t]/ab(rij))/ab(rij)
-    Dh2q2t = lambda rij, rik, q, t: \
-        d22h(rij, rik) * rik[:, q]/ab(rik) * rik[:, t]/ab(rik) \
-         + d2h(rij, rik) * (int(q == t) - rik[:, q]/ab(rik) * rik[:, t]/ab(rik))/ab(rik)
-    Dh1q2t = lambda rij, rik, q, t: \
-        d12h(rij, rik) * rij[:, q]/ab(rij) * rik[:, t]/ab(rik)
+    Dg2 = lambda rij, rik: (dg(costh(rij, rik)) * c2(rij, rik).T).T
+    Dg1 = lambda rij, rik: (dg(costh(rij, rik)) * c1(rij, rik).T).T
 
-    Dg1q = lambda rij, rik, q: dg(costh(rij, rik)) * c1q(rij, rik, q)
-    Dg2q = lambda rij, rik, q: dg(costh(rij, rik)) * c2q(rij, rik, q)
-
-    Dg1q1t = lambda rij, rik, q, t: \
-        (ddg(costh(rij, rik)) * c1q(rij, rik, q) * c1q(rij, rik, t)
-         + dg(costh(rij, rik)) * dc1q1t(rij, rik, q, t))
-    Dg2q2t = lambda rij, rik, q, t: \
-        (ddg(costh(rij, rik)) * c2q(rij, rik, q) * c2q(rij, rik, t)
-         + dg(costh(rij, rik)) * dc2q2t(rij, rik, q, t))
-    Dg1q2t = lambda rij, rik, q, t: \
-        (ddg(costh(rij, rik)) * c1q(rij, rik, q) * c2q(rij, rik, t)
-         + dg(costh(rij, rik)) * dc1q2t(rij, rik, q, t))
-
+    Dg11 = lambda rij, rik: \
+        (ddg(costh(rij, rik)) * (c1(rij, rik).reshape(-1, 3, 1) * c1(rij, rik).reshape(-1, 1, 3)).T
+         + dg(costh(rij, rik)) * dc11(rij, rik).T).T
+    
+    Dg22 = lambda rij, rik: \
+        (ddg(costh(rij, rik)) * (c2(rij, rik).reshape(-1, 3, 1) * c2(rij, rik).reshape(-1, 1, 3)).T
+         + dg(costh(rij, rik)) * dc22(rij, rik).T).T
+    Dg12 = lambda rij, rik: \
+        (ddg(costh(rij, rik)) * (c1(rij, rik).reshape(-1, 3, 1) * c2(rij, rik).reshape(-1, 1, 3)).T
+         + dg(costh(rij, rik)) * dc12(rij, rik).T).T
+        
     G = lambda rij, rik: g(costh(rij, rik)) * hf(rij, rik)
-    d1qG = lambda rij, rik, q: Dh1q(rij, rik, q) * g(costh(rij, rik)) + hf(rij, rik) * Dg1q(rij, rik, q)
-    d2qG = lambda rij, rik, q: Dh2q(rij, rik, q) * g(costh(rij, rik)) + hf(rij, rik) * Dg2q(rij, rik, q)
+    
+    d1G = lambda rij, rik: (Dh1(rij, rik).T * g(costh(rij, rik)) + hf(rij, rik) * Dg1(rij, rik).T).T
+    d2G = lambda rij, rik: (Dh2(rij, rik).T * g(costh(rij, rik)) + hf(rij, rik) * Dg2(rij, rik).T).T
 
-    d1q1tG = lambda rij, rik, q, t: \
-        Dg1q(rij, rik, q) * Dh1q(rij, rik, t) + Dg1q(rij, rik, t) * Dh1q(rij, rik, q) \
-        + g(costh(rij, rik)) * Dh1q1t(rij, rik, q, t) + hf(rij, rik) * Dg1q1t(rij, rik, q, t)
-    d2q2tG = lambda rij, rik, q, t: \
-        Dg2q(rij, rik, q) * Dh2q(rij, rik, t) + Dg2q(rij, rik, t) * Dh2q(rij, rik, q) \
-        + g(costh(rij, rik)) * Dh2q2t(rij, rik, q, t) + hf(rij, rik) * Dg2q2t(rij, rik, q, t)
-    d1q2tG = lambda rij, rik, q, t: \
-        Dg1q(rij, rik, q) * Dh2q(rij, rik, t) + Dg2q(rij, rik, t) * Dh1q(rij, rik, q) \
-        + g(costh(rij, rik)) * Dh1q2t(rij, rik, q, t) + hf(rij, rik) * Dg1q2t(rij, rik, q, t)
+    Dh2 = lambda rij, rik: (d2h(rij, rik) * rik.T / ab(rik)).T
+    Dh1 = lambda rij, rik: (d1h(rij, rik) * rij.T / ab(rij)).T
 
-    return {
-        'F': F,
-        'G': G,
-        'd1F': d1F,
-        'd2F': d2F,
-        'd11F': d11F,
-        'd12F': d12F,
-        'd22F': d22F,
-        'd1xG': lambda rij, rik: d1qG(rij, rik, 0),
-        'd1yG': lambda rij, rik: d1qG(rij, rik, 1),
-        'd1zG': lambda rij, rik: d1qG(rij, rik, 2),
-        'd2xG': lambda rij, rik: d2qG(rij, rik, 0),
-        'd2yG': lambda rij, rik: d2qG(rij, rik, 1),
-        'd2zG': lambda rij, rik: d2qG(rij, rik, 2),
-        'd1x1xG': lambda rij, rik: d1q1tG(rij, rik, 0, 0),
-        'd1y1yG': lambda rij, rik: d1q1tG(rij, rik, 1, 1),
-        'd1z1zG': lambda rij, rik: d1q1tG(rij, rik, 2, 2),
-        'd1y1zG': lambda rij, rik: d1q1tG(rij, rik, 1, 2),
-        'd1x1zG': lambda rij, rik: d1q1tG(rij, rik, 0, 2),
-        'd1x1yG': lambda rij, rik: d1q1tG(rij, rik, 0, 1),
-        'd2x2xG': lambda rij, rik: d2q2tG(rij, rik, 0, 0),
-        'd2y2yG': lambda rij, rik: d2q2tG(rij, rik, 1, 1),
-        'd2z2zG': lambda rij, rik: d2q2tG(rij, rik, 2, 2),
-        'd2y2zG': lambda rij, rik: d2q2tG(rij, rik, 1, 2),
-        'd2x2zG': lambda rij, rik: d2q2tG(rij, rik, 0, 2),
-        'd2x2yG': lambda rij, rik: d2q2tG(rij, rik, 0, 1),
-        'd1x2xG': lambda rij, rik: d1q2tG(rij, rik, 0, 0),
-        'd1y2yG': lambda rij, rik: d1q2tG(rij, rik, 1, 1),
-        'd1z2zG': lambda rij, rik: d1q2tG(rij, rik, 2, 2),
-        'd1y2zG': lambda rij, rik: d1q2tG(rij, rik, 1, 2),
-        'd1x2zG': lambda rij, rik: d1q2tG(rij, rik, 0, 2),
-        'd1x2yG': lambda rij, rik: d1q2tG(rij, rik, 0, 1),
-        'd1z2yG': lambda rij, rik: d1q2tG(rij, rik, 2, 1),
-        'd1z2xG': lambda rij, rik: d1q2tG(rij, rik, 2, 0),
-        'd1y2xG': lambda rij, rik: d1q2tG(rij, rik, 1, 0),
-        'cutoff': r2
-    }
+    Dh11 = lambda rij, rik: \
+        (d11h(rij, rik) * (((rij.reshape(-1, 3, 1) * rij.reshape(-1, 1, 3)).T/ab(rij)**2).T).T \
+         + d1h(rij, rik) * ((np.eye(3) - ((rij.reshape(-1, 3, 1) * rij.reshape(-1, 1, 3)).T/ab(rij)**2).T).T/ab(rij))).T
+    Dh22 = lambda rij, rik: \
+        (d22h(rij, rik) * (((rik.reshape(-1, 3, 1) * rik.reshape(-1, 1, 3)).T/ab(rik)**2).T).T \
+         + d2h(rij, rik) * ((np.eye(3) - ((rik.reshape(-1, 3, 1) * rik.reshape(-1, 1, 3)).T/ab(rik)**2).T).T/ab(rik))).T
+    Dh12 = lambda rij, rik: \
+        (d12h(rij, rik) * (rij.reshape(-1, 3, 1) * rik.reshape(-1, 1, 3)).T/(ab(rij)*ab(rik))).T
+        
+    d11G = lambda rij, rik: \
+        Dg1(rij, rik).reshape(-1, 3, 1) * Dh1(rij, rik).reshape(-1, 1, 3) + Dh1(rij, rik).reshape(-1, 3, 1) * Dg1(rij, rik).reshape(-1, 1, 3) \
+        + ((g(costh(rij, rik)) * Dh11(rij, rik).T).T + (hf(rij, rik) * Dg11(rij, rik).T).T)
+    d22G = lambda rij, rik: \
+        Dg2(rij, rik).reshape(-1, 3, 1) * Dh2(rij, rik).reshape(-1, 1, 3) + Dh2(rij, rik).reshape(-1, 3, 1) * Dg2(rij, rik).reshape(-1, 1, 3) \
+        + ((g(costh(rij, rik)) * Dh22(rij, rik).T).T + (hf(rij, rik) * Dg22(rij, rik).T).T)
+    d12G = lambda rij, rik: \
+        Dg1(rij, rik).reshape(-1, 3, 1) * Dh2(rij, rik).reshape(-1, 1, 3) + Dh1(rij, rik).reshape(-1, 3, 1) * Dg2(rij, rik).reshape(-1, 1, 3) \
+        + ((g(costh(rij, rik)) * Dh12(rij, rik).T).T + (hf(rij, rik) * Dg12(rij, rik).T).T)
+
     return {
         'F': F,
         'G': G,
@@ -188,14 +155,5 @@ def TersoffIII():
         'd11G': d11G,
         'd22G': d22G,
         'd12G': d12G,
-        'd1x2xG': lambda rij, rik: d1q2tG(rij, rik, 0, 0),
-        'd1y2yG': lambda rij, rik: d1q2tG(rij, rik, 1, 1),
-        'd1z2zG': lambda rij, rik: d1q2tG(rij, rik, 2, 2),
-        'd1y2zG': lambda rij, rik: d1q2tG(rij, rik, 1, 2),
-        'd1x2zG': lambda rij, rik: d1q2tG(rij, rik, 0, 2),
-        'd1x2yG': lambda rij, rik: d1q2tG(rij, rik, 0, 1),
-        'd1z2yG': lambda rij, rik: d1q2tG(rij, rik, 2, 1),
-        'd1z2xG': lambda rij, rik: d1q2tG(rij, rik, 2, 0),
-        'd1y2xG': lambda rij, rik: d1q2tG(rij, rik, 1, 0),
         'cutoff': R_2,
     }
