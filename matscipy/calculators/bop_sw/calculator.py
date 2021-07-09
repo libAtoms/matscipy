- Implemented non-affine forces from second derivatives - Test to verify correct implementation

matscipy/calculators/bop_sw/calculator.py
@@ -421,7 +421,34 @@ class AbellTersoffBrennerStillingerWeber(MatscipyCalculator):
 
         return H_nn / 2
 
-    """
+    def get_non_affine_forces_from_second_derivative(self, atoms):
+        if self.atoms is None:
+            self.atoms = atoms
+
+        i_p, j_p, r_p, r_pc = neighbour_list('ijdD', atoms=atoms, cutoff=2 * self.cutoff)
+
+        nb_atoms = len(self.atoms)
+        nb_pairs = len(i_p)
+
+        first_i = first_neighbours(nb_atoms, i_p)
+
+        naF_nccc = np.zeros((nb_atoms, 3, 3, 3))
+
+        for m in range(0, nb_atoms):
+            for cm in range(3):
+                drdb_pc = np.zeros((nb_pairs, 3))
+                drdb_pc[i_p == m, cm] = 1
+                drdb_pc[j_p == m, cm] = -1
+
+                for alpha in range(3):
+                    for beta in range(3):       
+                        drda_pc = np.zeros((nb_pairs, 3))
+                        drda_pc[first_i[m]:first_i[m+1], beta] = r_pc[first_i[m]:first_i[m+1], alpha]  
+                        naF_nccc[m, cm, alpha, beta] = self.get_second_derivative(atoms, drda_pc, drdb_pc,
+                                                                                  i_p=i_p, j_p=j_p, r_p=r_p, r_pc=r_pc) 
+        return naF_nccc
+
+"""
     def get_non_affine_forces(self, atoms):
         if self.atoms is None:
             self.atoms = atoms