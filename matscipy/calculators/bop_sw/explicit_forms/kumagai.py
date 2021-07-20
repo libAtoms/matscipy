ed8286def950d8d3658fc6131bbc95cfa5da1f47
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-07-20 17:30:54 +0200

    WIP: Non-affine forces

--- matscipy/calculators/bop_sw/explicit_forms/kumagai.py
+++ matscipy/calculators/bop_sw/explicit_forms/kumagai.py
@@ -5,7 +5,7 @@ kumagai_parameters = namedtuple("kumagai_parameters", ["A", "B", "lambda_1", "la
 
 # T. Kumagai et. al., Computational materials science 39.2 (2007): 457-464.
 kumagai = kumagai_parameters(A=3281.5905, B=121.00047, lambda_1=3.2300135, lambda_2=1.3457970, eta=1.0000000, delta=0.53298909, alpha=2.3890327,
-                    c_1=0.20173476, c_2=730418.72, c_3=1000000.0, c_4=1.0000000, c_5=26.000000, h=-0.36500000, R_1=2.70, R_2=3.30)
+                        c_1=0.20173476, c_2=730418.72, c_3=1000000.0, c_4=1.0000000, c_5=26.000000, h=-0.36500000, R_1=2.70, R_2=3.30)
 
 def ab(x):
     """
