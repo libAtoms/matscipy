6b16690efeef8f8f2342c0dcdecbc6145d42c4ee
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-07-28 15:08:57 +0200

    BUG: Actually test property MAINT: Output properties as table

--- tests/test_bulk_properties.py
+++ tests/test_bulk_properties.py
@@ -26,6 +26,7 @@ import pytest
 import numpy as np
 
 import ase.constraints
+from ase.lattice.compounds import B3
 from ase.lattice.cubic import Diamond
 from ase.optimize import FIRE
 from ase.units import GPa
@@ -37,107 +38,78 @@ from matscipy.calculators.manybody import Manybody
 from matscipy.calculators.manybody.explicit_forms import Kumagai, TersoffBrenner, StillingerWeber
 from matscipy.elasticity import full_3x3x3x3_to_Voigt_6x6
 
-
 ###
 
 sx = 1
 
 tests = [
-    ("Kumagai", Kumagai(kum.Kumagai_Comp_Mat_Sci_39_Si),
-     dict(name="dia-Si", struct=Diamond("Si", size=[sx,sx,sx]),
-            Ec=-4.630, a0=5.429, C11=166.4, C12=65.3, C44=77.1, C440=120.9)
-      ),
-    ("StillingerWeber", StillingerWeber(sw.Stillinger_Weber_PRB_31_5262_Si),
-      dict(name="dia-Si", struct=Diamond("Si", size=[sx,sx,sx]),
-            Ec=-4.630, a0=5.431, C11=161.6, C12=81.6, C44=60.3, C440=117.2, B=108.3)
-      ),
-    ("Tersoff3", TersoffBrenner(t3.Tersoff_PRB_39_5566_Si_C),
-     dict(name="dia-Si", struct=Diamond("Si", size=[sx,sx,sx]),
-             Ec=-4.63, a0=5.432, C11=143, C12=75, C44=69, C440=119, B=98)
-      )
-    ]
+    ("Kumagai-dia-Si", Kumagai(kum.Kumagai_Comp_Mat_Sci_39_Si),
+     dict(struct=Diamond("Si", size=[sx, sx, sx]),
+          Ec=4.630, a0=5.429, C11=166.4, C12=65.3, C44=77.1, C440=120.9)),
+    ("StillingerWeber-dia-Si", StillingerWeber(sw.Stillinger_Weber_PRB_31_5262_Si),
+     dict(struct=Diamond("Si", size=[sx, sx, sx]),
+          Ec=4.630, a0=5.431, C11=161.6, C12=81.6, C44=60.3, C440=117.2, B=108.3)),
+    ("Tersoff3-dia-C", TersoffBrenner(t3.Tersoff_PRB_39_5566_Si_C),
+     dict(struct=Diamond("C", size=[sx, sx, sx]),
+          Ec=7.396 - 0.0250, a0=3.566, C11=1067, C12=104, C44=636, C440=671)),
+    ("Tersoff3-dia-Si", TersoffBrenner(t3.Tersoff_PRB_39_5566_Si_C),
+     dict(struct=Diamond("Si", size=[sx, sx, sx]),
+          Ec=4.63, a0=5.432, C11=143, C12=75, C44=69, C440=119, B=98)),
+    ("Tersoff3-dia-Si-C", TersoffBrenner(t3.Tersoff_PRB_39_5566_Si_C),
+     dict(struct=B3(["Si", "C"], latticeconstant=4.3596, size=[sx, sx, sx]),
+          Ec=6.165, a0=4.321, C11=437, C12=118, C440=311, B=224)),
+]
 
 
 @pytest.mark.parametrize('test', tests)
 def test_cubic_elastic_constants(test):
-    pot, par, mats = test
+    name, pot, mat = test
 
-    calculator = Manybody(**par)
-    atoms = mats["struct"]
+    calculator = Manybody(**pot)
+    atoms = mat["struct"]
     atoms.translate([0.1, 0.1, 0.1])
     atoms.set_scaled_positions(atoms.get_scaled_positions())
     atoms.set_calculator(calculator)
 
     FIRE(
-        ase.constraints.StrainFilter(atoms, mask=[1,1,1,0,0,0]),
+        ase.constraints.StrainFilter(atoms, mask=[1, 1, 1, 0, 0, 0]),
         logfile=None).run(fmax=0.0001)
 
     # Ec
-    Ec = atoms.get_potential_energy()/len(atoms)
-    if "Ec" in mats:
-        np.isclose(Ec, mats["Ec"], atol=1e-8, rtol=1e-8)
-    else:
-        print("Ec = ", Ec, " eV")
+    Ec = -atoms.get_potential_energy() / len(atoms)
 
     # a0
     c1, c2, c3 = atoms.get_cell()
-    a0 = np.sqrt(np.dot(c1, c1))/sx
-    if "a0" in mats:
-        np.isclose(a0, mats["a0"], atol=1e-8, rtol=1e-8)
-    else:
-        print("a0 = ", a0, " A")
-
+    a0 = np.sqrt(np.dot(c1, c1)) / sx
 
     # C11, C12, C44
     Caffine = calculator.get_birch_coefficients(atoms)
     Cnonaffine = calculator.get_non_affine_contribution_to_elastic_constants(atoms, tol=1e-5)
-    C =  full_3x3x3x3_to_Voigt_6x6(Caffine+Cnonaffine)
-    C11 = C[0,0]/GPa
-    C12 = C[0,1]/GPa
-    C44 = C[3,3]/GPa
-    C110 = full_3x3x3x3_to_Voigt_6x6(Caffine)[0,0]/GPa
-    C120 = full_3x3x3x3_to_Voigt_6x6(Caffine)[0,1]/GPa
-    C440 = full_3x3x3x3_to_Voigt_6x6(Caffine)[3,3]/GPa
-    B  = (C11+2*C12)/3
+    C = full_3x3x3x3_to_Voigt_6x6(Caffine + Cnonaffine)
+    C11 = C[0, 0] / GPa
+    C12 = C[0, 1] / GPa
+    C44 = C[3, 3] / GPa
+    C110 = full_3x3x3x3_to_Voigt_6x6(Caffine)[0, 0] / GPa
+    C120 = full_3x3x3x3_to_Voigt_6x6(Caffine)[0, 1] / GPa
+    C440 = full_3x3x3x3_to_Voigt_6x6(Caffine)[3, 3] / GPa
+    B = (C11 + 2 * C12) / 3
 
-    if "C11" in mats:
-        np.isclose(C11, mats["C11"], atol=1e-10, rtol=1e-8)
-        #print("C11 = ", C11, " GPa")
-    else:
-        print("C11 = ", C11, " GPa")
+    print()
+    print(f'=== {name}===')
+    l1 = f'          '
+    l2 = f'Computed: '
+    l3 = f'Reference:'
+    for prop in ['Ec', 'a0', 'C11', 'C110', 'C12', 'C120', 'C44', 'C440', 'B']:
+        l1 += prop.rjust(11)
+        l2 += f'{locals()[prop]:.2f}'.rjust(11)
+        if prop in mat:
+            l3 += f'{float(mat[prop]):.2f}'.rjust(11)
+        else:
+            l3 += '---'.rjust(11)
+    print(l1)
+    print(l2)
+    print(l3)
 
-    if "C12" in mats:
-        np.isclose(C12, mats["C12"], atol=1e-10, rtol=1e-8)
-        #print("C12 = ", C12, " GPa")
-    else:
-        print("C12 = ", C12, " GPa")
-
-    if "C44" in mats:
-        np.isclose(C44, mats["C44"], atol=1e-10, rtol=1e-8)
-        #print("C44 = ", C44, " GPa")
-    else:
-        print("C44 = ", C44, " GPa")
-
-    if "C110" in mats:
-        np.isclose(C110, mats["C110"], atol=1e-10, rtol=1e-8)
-        #print("C110 = ", C110, " GPa")
-    else:
-        print("C110 = ", C110, " GPa")
-
-    if "C120" in mats:
-        np.isclose(C120, mats["C120"], atol=1e-10, rtol=1e-8)
-        #print("C120 = ", C120, " GPa")
-    else:
-        print("C120 = ", C120, " GPa")
-
-    if "C440" in mats:
-        np.isclose(C440, mats["C440"], atol=1e-10, rtol=1e-8)
-        #print("C440 = ", C440, " GPa")
-    else:
-        print("C440 = ", C440, " GPa")
-
-    if "B" in mats:
-        np.isclose(B, mats["C440"], atol=1e-10, rtol=1e-8)
-        #print("C440 = ", C440, " GPa")
-    else:
-        print("B = ", B, " GPa")
+    for prop, value in mat.items():
+        if prop != 'struct':
+            np.testing.assert_allclose(locals()[prop], value, rtol=0.1)
