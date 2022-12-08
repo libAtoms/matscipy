206eaa8250f6639b2de73d0fbf444b348acaa698
Lucas Frérot <lucas.frerot@imtek.uni-freiburg.de>
2022-12-08 18:13:20 +0100

    TST: birch coefficients

--- tests/manybody/heterogeneous/polyphosphate.py
+++ tests/manybody/heterogeneous/polyphosphate.py
@@ -1,5 +1,7 @@
+import pytest
 import numpy as np
 
+from numpy.linalg import norm
 from ase.io import read
 from ase.calculators.mixing import SumCalculator
 from matscipy.molecules import Molecules
@@ -36,7 +38,12 @@ def set_lammps(atoms, lammps_datafile):
 
     # These commands to accomodate for ASE
     change_box all triclinic
-    kspace_style ewald 1e-4
+    kspace_style ewald 1e-12
+
+    # Deactivate pair and coulomb
+    kspace_style none
+    pair_style lj/cut 10
+    pair_coeff * * 0 1
     """.split("\n")
 
     calc = LAMMPSlib(lmpcmds=[],
@@ -45,7 +52,8 @@ def set_lammps(atoms, lammps_datafile):
                      create_atoms=False,
                      create_box=False,
                      boundary=False,
-                     keep_alive=True)
+                     keep_alive=True,
+                     log_file='log.lammps')
     atoms.calc = calc
 
 
@@ -88,7 +96,7 @@ def set_manybody(atoms, molecules):
     }, neigh)
 
     ewald = Ewald()
-    ewald.set(accuracy=1e-4, cutoff=10., verbose=False)
+    ewald.set(accuracy=1e-12, cutoff=10., verbose=False)
     atoms.arrays['charge'] = atoms.get_initial_charges()
 
     atoms.calc = SumCalculator([pair, ewald, triplet])
@@ -108,7 +116,8 @@ def map_ase_types(atoms):
     return atoms
 
 
-def test_angles_energy():
+@pytest.fixture
+def polyphosphate():
     atoms = read('polyphosphate.data',
                  format='lammps-data',
                  style='full',
@@ -120,7 +129,11 @@ def test_angles_energy():
         1: HarmonicAngle(2 * 1.77005, np.radians(109.47)),
         2: HarmonicAngle(2 * 10.4663, np.radians(135.58)),
     }, MolecularNeighbourhood(mol))
+    return atoms, mol
 
+
+def test_angles_energy(polyphosphate):
+    atoms, mol = polyphosphate
     epot = atoms.get_potential_energy()
 
     epot_ref = 0
@@ -131,5 +144,30 @@ def test_angles_energy():
     assert np.abs(epot_ref - epot) / epot_ref < 1e-14
 
 
-if __name__ == "__main__":
-    test_angles_energy()
+def test_affine_elastic_constants(polyphosphate):
+    from matscipy.elasticity import \
+        measure_triclinic_elastic_constants as num_constants
+
+    atoms, _ = polyphosphate
+
+    # So that Cauchy stress is non-zero
+    atoms.cell *= 0.8
+    atoms.positions *= 0.8
+
+    # Numerical Birch
+    C_many_ref = num_constants(atoms, delta=1e-2)
+
+    # Consistency test if LAMMPS is installed
+    try:
+        latoms = atoms.copy()
+        set_lammps(latoms, 'polyphosphate.data')
+        C_lammps = num_constants(latoms, delta=1e-2, verbose=False)
+        assert norm(C_lammps - C_many_ref) / norm(C_lammps) < 1e-7
+    except AssertionError as e:
+        raise(e)
+    except Exception as e:
+        print(e)
+
+    C_birch = atoms.calc.get_property('birch_coefficients', atoms)
+    assert norm(C_birch - C_many_ref) / norm(C_many_ref) < 1e-2
+    np.testing.assert_allclose(C_birch, C_many_ref, atol=3e-5, rtol=1e-3)
