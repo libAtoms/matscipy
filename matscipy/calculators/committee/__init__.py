ebc85a4a792965e946c1b9ae06281517fa7380d3
Simon Wengert <wengert@fhi.mpg.de>
2023-04-05 15:28:39 +0200

    ENH: added __init__.py and updated meson.build for committee approach implementation

---
+++ matscipy/calculators/committee/__init__.py
@@ -0,0 +1,5 @@
+"""Implements the Committee (of Models) approach."""
+
+from .committee import CommitteeUncertainty, Committee, CommitteeMember
+from .log import set_logging
+from .utils import subsample
