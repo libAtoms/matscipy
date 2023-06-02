8526a2d5b263dc81db0d0df213f742146bb8ebd5
Fraser-Birks <fraser.birks@warwick.ac.uk>
2023-06-02 13:36:05 +0100

    WIP: Removed options for 3rd and 4th derivatives in shift corrector

--- matscipy/cauchy_born.py
+++ matscipy/cauchy_born.py
@@ -8,12 +8,11 @@ from scipy.linalg import polar, sqrtm
 from itertools import permutations
 
 class CubicCauchyBorn:
-    def __init__(self,el,a0,calc,lattice=Diamond,term=2):
+    def __init__(self,el,a0,calc,lattice=Diamond):
         self.el = el
         self.a0 = a0
         self.calc = calc
         self.lattice = lattice
-        self.num_terms = term
 
     def set_sublattices(self,atoms,A):
         #currently, the idea for this is to apply a small strain to atoms
@@ -125,21 +124,6 @@ class CubicCauchyBorn:
             return (find_grad(eps_up,i,calc,unitcell)-find_grad(eps_down,i,calc,unitcell))/(2*self.de)
 
 
-        def find_third_deriv(eps,i,j,k,*args):
-            eps_up = eps.copy()
-            eps_down = eps.copy()
-            eps_up[k]+=self.de
-            eps_down[k]-=self.de
-            return (find_second_deriv(eps_up,i,j,calc,unitcell)-find_second_deriv(eps_down,i,j,calc,unitcell))/(2*self.de)
-
-        def find_fourth_deriv(eps,i,j,k,l,*args):
-            eps_up = eps.copy()
-            eps_down = eps.copy()
-            eps_up[l]+=self.de
-            eps_down[l]-=self.de
-            return (find_third_deriv(eps_up,i,j,k,calc,unitcell)-find_third_deriv(eps_down,i,j,k,calc,unitcell))/(2*self.de)
-
-
         self.de = de
         unitcell = self.lattice(self.el, latticeconstant=self.a0, size=[1, 1, 1])
         unitcell.set_pbc([True,True,True])
@@ -147,8 +131,6 @@ class CubicCauchyBorn:
         #compute all the terms we need:
         grad_f = np.zeros([6])
         hess_f = np.zeros([6,6])
-        third_deriv_f = np.zeros([6,6,6])
-        fourth_deriv_f = np.zeros([6,6,6,6])
         eps_vec = np.zeros([6])
         for i in range(6):
             grad_f[i] = find_grad(eps_vec,i,calc,unitcell)
@@ -157,32 +139,15 @@ class CubicCauchyBorn:
                 hess_f[i,j] = find_second_deriv(eps_vec,i,j,calc,unitcell)
                 #include relevant symmetries
                 hess_f[j,i] = hess_f[i,j]
-                for k in range(6-j):
-                    third_deriv_f[i,j,k] = find_third_deriv(eps_vec,i,j,k,calc,unitcell)
-                    third_deriv_f[i,k,j] = third_deriv_f[i,j,k]
-                    third_deriv_f[j,i,k] = third_deriv_f[i,j,k]
-                    third_deriv_f[j,k,i] = third_deriv_f[i,j,k]
-                    third_deriv_f[k,i,j] = third_deriv_f[i,j,k]
-                    third_deriv_f[k,j,i] = third_deriv_f[i,j,k]
-                    for l in range(6-k):
-                        fourth_deriv_f[i,j,k,l] = find_fourth_deriv(eps_vec,i,j,k,l,calc,unitcell)
-                        a = [i,j,k,l]
-                        perms = set(permutations(a))
-                        for p in perms:
-                            (m,n,o,p) = p
-                            fourth_deriv_f[m,n,o,p] = fourth_deriv_f[i,j,k,l]
 
         self.grad_f = grad_f
         self.hess_f = hess_f
-        self.third_deriv_f = third_deriv_f
-        self.fourth_deriv_f = fourth_deriv_f
+
 
     def save_taylor(self):
         try:
             np.save('grad_f.npy',self.grad_f)
             np.save('hess_f.npy',self.hess_f)
-            np.save('third_deriv_f.npy',self.third_deriv_f)
-            np.save('fourth_deriv_f.npy',self.fourth_deriv_f)
         except AttributeError:
             print('Need to fit a model with fit taylor before it can be saved!')
 
@@ -190,8 +155,6 @@ class CubicCauchyBorn:
         try:
             self.grad_f = np.load('grad_f.npy')
             self.hess_f = np.load('hess_f.npy')
-            self.third_deriv_f = np.load('third_deriv_f.npy')
-            self.fourth_deriv_f = np.load('fourth_deriv_f.npy')
         except FileNotFoundError:
             print('No files found containing model parameters - make sure to call save_taylor() to save the model.')
         
@@ -327,8 +290,6 @@ class CubicCauchyBorn:
         def eval_tay_model(self,eps):
             grad_f = self.grad_f
             hess_f = self.hess_f
-            third_deriv_f = self.third_deriv_f
-            fourth_deriv_f = self.fourth_deriv_f
             #takes E as a 2D array of voigt vectors, with number of rows=number atoms
             # Note that the permutation is computed from observing how the matrix components permute
             # when transforming the strain matrix by the 120 degree rotation matrices about [111],
@@ -342,32 +303,21 @@ class CubicCauchyBorn:
                     strain_vec_perm[:,perm_num+3] = strain_vec[:,i+3]
                 return strain_vec_perm
 
-            def eval_eps(eps_vec,grad_f,hess_f,third_deriv_f):
+            def eval_eps(eps_vec,grad_f,hess_f):
                 term_1 = grad_f@eps_vec
                 term_2 = (1/2)*(np.transpose(eps_vec)@hess_f@eps_vec)
-                term_3 = (1/6)*(np.einsum('ijk,i,j,k',third_deriv_f,eps_vec,eps_vec,eps_vec))
-                term_4 = (1/24)*(np.einsum('ijkl,i,j,k,l',fourth_deriv_f,eps_vec,eps_vec,eps_vec,eps_vec))
 
-                if self.num_terms == 1:
-                    return term_1
-                elif self.num_terms == 2:
-                    return term_1 + term_2
-                elif self.num_terms == 3:
-                    return term_1 + term_2 + term_3
-                elif self.num_terms == 4:
-                    return term_1 + term_2 + term_3 + term_4
-                else:
-                    print('Error, can only return 1, 2, 3 or 4 terms')
-                    raise ValueError
+                return term_1 + term_2
+
 
             epsx = eps
             epsy = permutation(eps,1)
             epsz = permutation(eps,2)
             predictions = np.zeros([np.shape(eps)[0],3])
             for i in range(np.shape(eps)[0]):
-                predictions[i,0] = eval_eps(epsx[i,:],grad_f,hess_f,third_deriv_f)
-                predictions[i,1] = eval_eps(epsy[i,:],grad_f,hess_f,third_deriv_f)
-                predictions[i,2] = eval_eps(epsz[i,:],grad_f,hess_f,third_deriv_f)
+                predictions[i,0] = eval_eps(epsx[i,:],grad_f,hess_f)
+                predictions[i,1] = eval_eps(epsy[i,:],grad_f,hess_f)
+                predictions[i,2] = eval_eps(epsz[i,:],grad_f,hess_f)
             
             return predictions
 
