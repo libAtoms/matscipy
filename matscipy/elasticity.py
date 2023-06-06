1d0c2f679463598c172688e2f3771734b680d2f6
Kristof Bal <kristof.bal@gmail.com>
2023-06-06 18:55:29 +0200

    Avoid rotating x into itself.
    Previous solution did not use normalized vectors, not
    catching the general [a,0,0] case.
    Also, numpy.allclose() does not catch the [-a,0,0] case.

--- matscipy/elasticity.py
+++ matscipy/elasticity.py
@@ -1100,7 +1100,7 @@ def elastic_moduli(C, l=np.array([1, 0, 0]), R=None, tol=1e-6):
         u_b = np.array([1, 0, 0])
         R = np.eye(3)
 
-        if not np.allclose(l, u_b, rtol=tol, atol=tol):
+        if np.abs(np.dot(u_a, u_b)) < 1.0 - tol:
             u_v = np.cross(u_a, u_b)
             u_v_mat = np.array([[ 0,      -u_v[2], u_v[1]],
                                 [ u_v[2], 0,      -u_v[0]],
