fe0e49ea30fe2d455c564903668eb07bef28dc9a
Petr Grigorev <grigorievpit@gmail.com>
2023-11-03 19:53:04 +0100

    DOC: added copybutton

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,8 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.mathjax',
     'myst_nb',
-    'numpydoc'
+    'numpydoc',
+    'sphinx_copybutton',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
